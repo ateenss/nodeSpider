1acca8760cd09cc37e16ca92849d1d53c7143cc8
ateenss <1786851090@qq.com>
2019-10-04 23:26:41 +0800

    前端补充

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import PrivateRoute from './components/PrivateRoute'
-import {Route,Switch} from 'react-router-dom'
+import {  Route,Switch} from 'react-router-dom'
 import Login from './routes/Logins/index'
 // import Login from './routes/Login2/index'
 import Index from './routes/Index/index'
@@ -10,10 +10,10 @@ import './assets/font/iconfont.css'
 class App extends Component {
   render() {
     return (
-        <Switch>
-          <Route path='/login' component={Login}/>
-          <PrivateRoute path='/' component={Index}/>
-        </Switch>
+            <Switch>
+                <Route path='/login' component={Login}/>
+                <PrivateRoute path='/' component={Index}/>
+            </Switch>
     )
   }
 }
--- front/src/components/PrivateRoute/index.js
+++ front/src/components/PrivateRoute/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Redirect, } from 'react-router-dom'
+import { Route, Redirect} from 'react-router-dom'
 import { isAuthenticated } from '../../utils/Session'
 
 const PrivateRoute = ({component: Component, ...rest}) => (
