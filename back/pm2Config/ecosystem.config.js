df1054915584650dfa252d5cfcbfd2cafbf24629
ateenss <1786851090@qq.com>
2019-10-05 19:21:16 +0800

    前端补充

---
+++ back/pm2Config/ecosystem.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  apps : [{
+    name: 'nodeSpider',
+    // script: 'app.js',
+    script: '../build/backend.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    error_file:"./pm2Logs/error.log",
+    out_file:"./pm2Logs/out.log",
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    },
+
+  }],
+
+  deploy : {
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
--- back/webpack.config.js
+++ back/webpack.config.js
@@ -1,7 +1,6 @@
 let webpack = require('webpack');
 let path = require('path');
 let fs = require('fs');
-let UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 let nodeModules = {};
 fs.readdirSync('node_modules')
@@ -27,22 +26,6 @@ module.exports = {
                 banner:'require("source-map-support").install();',
                 raw: true,
                 entryOnly: false }),
-        new webpack.ProgressPlugin({
-            entries: true,
-            modules: true,
-            modulesCount: 100,
-            profile: true,
-            handler: (percentage, message, ...args) => {
-                console.info(percentage, message, ...args);
-            }
-        }),
-        new UglifyJsPlugin({
-            test: /\.js(\?.*)?$/i,
-            include: /\/includes/,
-            exclude: /\/node_modules\/*/,
-            cache: true,
-            parallel: true,
-        }),
     ],
     mode:'production',
     devtool: 'sourcemap'
